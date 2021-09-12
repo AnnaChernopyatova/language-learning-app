e9fa5f1412a14a6acccc0ed34594977a94ab8977
AnnaChernopyatova <anna.chernopyatova913@mail.ru>
2021-09-12 16:00:04 +0300

    work with life cycle

--- src/components/cardsEnded.jsx
+++ src/components/cardsEnded.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import workDone from './images/workDone.jpg'
+import styles from './styles/cardsEnded.css';
 
-function CardsEnded(props){
+function CardsEnded(handlenumber){
+    console.log(handlenumber.handlenumber);
 
     return(
-        <div>
-            Карточки закончились. Вы молодец!
-            <button onClick={()=>props.handlenumber}>
-                Начать сначала.
+        <div className='cardsFinished'>
+            <h2 className='cardsFinished_header'>
+                Карточки закончились. Вы молодец!
+            </h2>
+            <img alt='Done work' src={workDone} className='cardsFinished_pic'/>
+            <button className='cardsFinished_button' onClick={()=>handlenumber.handlenumber()}>
+                Начать сначала
             </button>
         </div>
     )
